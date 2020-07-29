364d28f42a1180cf837d3c3b91ae393c9aad1bd4
a1744771 <a1744771@bc9194f5-9013-45db-a6f7-1d9dcf0531ff>
2020-07-29 06:04:11 +0000

    OOP: Added solution to problem 2-5

---
+++ 2020/s2/oop/practical-01/function-2-5.cpp
@@ -0,0 +1,15 @@
+bool decending(int array[], int n){
+	if(n < 1){
+		return false;
+	}
+
+	bool dsc = true;
+
+	for(int i = 1; i < n; i++){
+		if(array[i] > array[i-1]){
+			dsc = false;
+		}
+	}
+
+	return dsc;
+}
---
+++ 2020/s2/oop/practical-01/main-2-5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+extern bool decending(int[], int);
+
+int main(){
+	int array1[] = {10,9,8,7,6,5,4,3,2,1};//decending
+	int array2[] = {15,12,14,11,13,10,3,10,5,1};//not decending
+
+	cout << "Array is in decending order: ";
+
+	if(decending(array1,10) == true){
+		cout << "True" << endl;
+	}else{
+		cout << "False"<< endl;
+	}
+ 
+
+	return 0;
+}
