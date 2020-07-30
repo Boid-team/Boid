528728af45a6676578c12b0ba9452aa968c2adb4
a1744771 <a1744771@bc9194f5-9013-45db-a6f7-1d9dcf0531ff>
2020-07-30 05:45:00 +0000

    updated function and main 3-3

--- 2020/s2/oop/practical-01/main-3-3.cpp
+++ 2020/s2/oop/practical-01/main-3-3.cpp
@@ -4,9 +4,9 @@ using namespace std;
 extern double weightedaverage(int[], int);
 
 int main(){
-	int array1[] = {1,2,1,4,3,6};	
+	int array1[] = {1,2,1,4,3,1};	
 	int array2[] = {1,1,1,1,1,1};
-	int array3[] = {};
+	int array3[] = {6,5,4,2,2,4,5,6};
 
-	cout << "The weighted average of the array is: " << weightedaverage(array2, 6) << endl;
+	cout << "The weighted average of the array is: " << weightedaverage(array3, 8) << endl;
 }
