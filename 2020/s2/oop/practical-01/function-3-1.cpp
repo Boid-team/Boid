473e2f0b588eaa1212aa8ebe65a37a8a7286f79a
a1744771 <a1744771@bc9194f5-9013-45db-a6f7-1d9dcf0531ff>
2020-07-30 05:50:47 +0000

    Updated function-3-1

--- 2020/s2/oop/practical-01/function-3-1.cpp
+++ 2020/s2/oop/practical-01/function-3-1.cpp
@@ -1,6 +1,6 @@
 bool fanarray(int array[], int n){
 	
-	bool isFan = true;;
+	bool isFan = true;
 
 	if(n<1){
 		isFan = false;
@@ -9,7 +9,7 @@ bool fanarray(int array[], int n){
 	if(n % 2 == 0){//if number of elements is even
 
 		for(int i = 0; i < n/2;i++){
-			if(!(array[i] == array[n-1-i])){
+			if(array[i] != array[n-1-i]){
 				isFan = false;
 			}
 
@@ -17,7 +17,7 @@ bool fanarray(int array[], int n){
 
 	}else{//if number of elements is odd
 		for(int i = 0; i < (n-1) / 2; i++){
-			if(!(array[i] == array[n-1-i])){
+			if(array[i] != array[n-1-i]){
 				isFan = false;
 			}
 		}
