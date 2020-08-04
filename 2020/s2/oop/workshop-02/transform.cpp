6068ae5191944585550f4c0f4c4d4c6096e12606
a1744771 <a1744771@bc9194f5-9013-45db-a6f7-1d9dcf0531ff>
2020-08-04 05:43:52 +0000

    OOP: Wrote a program to generate binary numbers

---
+++ 2020/s2/oop/workshop-02/transform.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+
+	int I = 2;
+
+	std::vector<int> vec = {};
+
+	while(I != 0){
+		vec.push_back(I % 2);
+		I = I/2;
+	}
+
+	for(int i = vec.size()-1; i >= 0; i--){
+		std::cout << vec[i];
+	}
+	
+	std::cout << std::endl;
+
+	return 0;
+}
