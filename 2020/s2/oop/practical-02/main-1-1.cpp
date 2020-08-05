aed2a654b3f3272d9babf561d9c4e922de987b8d
a1744771 <a1744771@bc9194f5-9013-45db-a6f7-1d9dcf0531ff>
2020-08-05 01:23:54 +0000

    OOP: Added solution to problem 1-1

---
+++ 2020/s2/oop/practical-02/main-1-1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+extern int diagonal(int[4][4]);
+
+int main(){
+
+	int array[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+	/* array looks like:
+
+	{ 1  2  3  4
+	  5  6  7  8	
+	  9  10 11 12	
+	  13 14 15 16
+	}
+
+
+	*/
+	
+	printf("Sum of diagonal values in array: %d\n", diagonal(array));
+}
