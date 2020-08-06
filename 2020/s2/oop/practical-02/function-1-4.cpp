662d6b9c4dc5ef8172bfcaf34bbde80e1fdff03f
a1744771 <a1744771@bc9194f5-9013-45db-a6f7-1d9dcf0531ff>
2020-08-06 04:44:33 +0000

    Added main-1-4.cpp and updated function-1-4 for final solution to problem 1-4

--- 2020/s2/oop/practical-02/function-1-4.cpp
+++ 2020/s2/oop/practical-02/function-1-4.cpp
@@ -1,7 +1,20 @@
+#include <iostream>
+using namespace std;
+
 void print_scaled_matrix(int array[3][3], int scale){
 
-	
+	int arr[3][3]; 
 
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			arr[i][j] = array[i][j];
+			arr[i][j] *= 3;
 
+			cout << arr[i][j] << " ";
+
+		}
+
+		cout << endl;
+	}
 	
 }
