df23be453670bb707a8f320d21ac3581beca4ea7
a1744771 <a1744771@bc9194f5-9013-45db-a6f7-1d9dcf0531ff>
2020-08-11 05:45:33 +0000

    Added main-1-3, remember to add makefile later

---
+++ 2020/s2/oop/workshop-03/main-1-3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+extern void print_class(std::string[4], std::string[], int[][4],int);
+
+int main(){
+
+	std::string courses[4];
+
+	std::cout << "Enter names of courses: " <<std:endl;
+	std::cin >> courses;
+
+	std::cout << "Enter number of students: "
+	std::string students[]
+	std::cin >> students;
+
+
+
+}
