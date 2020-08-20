8b76417df7ff1d69ec02791ed64a70f1d3524ec9
a1744771 <a1744771@bc9194f5-9013-45db-a6f7-1d9dcf0531ff>
2020-08-20 05:19:14 +0000

    Updated function-1-3.cpp

--- 2020/s2/oop/practical-04/function-1-3.cpp
+++ 2020/s2/oop/practical-04/function-1-3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void cpyia(int *ptrtr, int *newPtr], int length){
+void cpyia(int *ptr, int *newPtr, int length){
 	for(int i = 0; i < length; i++){
 		*(newPtr+i) = *(ptr + i);
 	}
