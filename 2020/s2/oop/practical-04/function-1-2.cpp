287229b88ed2b39939bfb63e3fe52f3c27abdb92
a1744771 <a1744771@bc9194f5-9013-45db-a6f7-1d9dcf0531ff>
2020-08-20 02:27:12 +0000

    Added comments to sample code in function-1-2.cpp

---
+++ 2020/s2/oop/practical-04/function-1-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+//
+int main()
+{
+	int *ptr, a[10]={1,2,3,4,5,6,7,8,9,10};
+	int *b = new int[10];
+
+	/* address of a[0] is assigned to ptr */
+	ptr = &a[0];   
+
+	//The code is assigning the values ponted to by b to the values at the pointer a
+	b = ptr;
+
+	cout << "Value of first element in b " << b[0] << endl; 
+	cout << "Value of second element in b " << b[1] << endl; 
+
+	a[1] = 42;
+	cout << "Value of second element in b " << b[1] << endl;
+
+	return 0;
+}
