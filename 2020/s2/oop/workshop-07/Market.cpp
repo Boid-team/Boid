02cf6602ae38ae86680a4ab55b2b9b44b8448e3b
a1744771 <a1744771@bc9194f5-9013-45db-a6f7-1d9dcf0531ff>
2020-09-08 05:22:40 +0000

    Added solution to problem 4-1

---
+++ 2020/s2/oop/workshop-07/Market.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include 'Market.h'
+using namespace std;
+
+Market::Market(){
+	name = "Default name";
+	age = 0;
+}
+
+void Market::setName(string marName){
+	name = marName;
+}
+
+string Market::getName(){
+	return name;
+}
+
+void Market::setAge(int merAge){
+	age = merAge;
+}
+
+int Market::getAge(){
+	return age;
+}
---
+++ 2020/s2/oop/workshop-07/Market.h
@@ -0,0 +1,21 @@
+#ifndef _MARKET_H__
+#define _MARKET_H__
+
+#include <string>
+using namespace std;
+
+class Market{
+	string name;
+	int age;
+public:
+	Market();
+	void setname(string);
+	string getName();
+	void setAge(int);
+	int getAge();
+
+};
+
+
+
+#endif _MARKET_H__
