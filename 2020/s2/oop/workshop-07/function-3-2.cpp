27645f101c47a0a91e5967ab4251e72a682223a1
a1744771 <a1744771@bc9194f5-9013-45db-a6f7-1d9dcf0531ff>
2020-09-08 05:17:19 +0000

    added solution to problem 3-2

---
+++ 2020/s2/oop/workshop-07/function-3-2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int *readNumbers(){
+	int *array = new int[10];
+	int n;
+	for(int i = 0; i < 10; i++){
+		cin >> n;
+		*(array + i) = n;
+	}
+
+	return array;
+}
+
+void printNumbers(int *numbers, int length){
+	for(int i = 0; i < length; i++){
+		cout << i << " " << *(numbers + i) << endl;
+	}
+}
+
+bool equalsArray(int *numbers1, int *numbers2, int length){
+
+	if(length < 1){
+		return 0;
+	}
+
+	int count = 0;
+	for(int i = 0; i < length; i++){
+		if(*(numbers1 + i) == *(numbers2 + i)){
+			count++;
+		}
+	}
+	if(count == length){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+int *reverseArray(int *numbers1, int length){
+	int *array = new int[length];
+	for(int i = 0; i < length; i++){
+		*(array + i) = *(numbers1 + length - 1 - i);
+	}
+	return array;
+}
