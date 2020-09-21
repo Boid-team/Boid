cb4f5880ceec0eabf6342eb69412ecbad5aecf3d
a1744771 <a1744771@bc9194f5-9013-45db-a6f7-1d9dcf0531ff>
2020-09-21 08:55:36 +0000

    updated main.cpp to include loop

--- 2020/s2/oop/Maj-practical/main.cpp
+++ 2020/s2/oop/Maj-practical/main.cpp
@@ -1,38 +1,49 @@
 #include <ncurses.h>
-#include "Boid.h"
+#include "Object.h"
 using namespace std;
 int main(){
 
-	bool running = false;
-
+	cout << "enter type of boid: " << endl;
 	cin >> type;
 
 	if(type == "bird"){
 		for(int i = 0; i < 10; i++){
 			Boid array = new Boid();
 		}
-		running = true;
 	}else if(type == "fish"){
-
+		for(int i = 0; i < 10; i++){
+			Boid array = new Boid();
+		}
 	}
 	initscr();
-	while(running == true){
+	// while(running == true){
 
-		for(int i =0; i < 10; i++){
+	// 	for(int i =0; i < 10; i++){
 
-			array[i].checkEdges();
-			array[i].checkCoherence();
-			array[i].checkSeparation();
-			array[i].checkAlignment();
+	// 		array[i].checkEdges();
+	// 		array[i].checkCoherence();
+	// 		array[i].checkSeparation();
+	// 		array[i].checkAlignment();
 
-			array[i].updatePos();
-			draw(array[i].gety, array[i]. getx);
+	// 		array[i].updatePos();
+	// 		draw(array[i].gety, array[i]. getx);
+	// 	}
+	// }
+	char ch;
+	int i = 0;
+	while(1){
+		if(ch == 'q' | ch == 'Q'){
+			break;
+		}else{
+
+			
+			
+			ch = getch();
+			clear();
 		}
-
-		
-
-
 	}
+
+
 	endwin();
 	return 0;
 }
